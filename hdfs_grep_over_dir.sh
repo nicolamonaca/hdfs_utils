1a769bfbdd3abb0ac8147de1531895ac73c6ab66
Nicola Lamonaca <nicola.lamonaca@gmail.com>
2017-07-13 16:31:01 +0200

    Update hdfs_grep_over_dir.sh

--- hdfs_grep_over_dir.sh
+++ hdfs_grep_over_dir.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+hdfs_dir=$1
+string=$2
 
-hdfs dfs -ls $1 | awk '{print $8}' | \
-  xargs -n 1 -I ^ -P 10 bash -c \ #Parallelize by downloading and grepping 10 files at a time
-  "hdfs dfs -cat ^ | grep -q '$2' && echo ^"
+# Determine the optimal parallelization factor
+num_files=$(hdfs dfs -ls $hdfs_dir | wc -l)
+p_factor=$((($num_files - 1) / 3))
+
+#Parallelize by downloading $p_factor files at a time
+hdfs dfs -ls $hdfs_dir | awk '{print $8}' | \
+  xargs -n 1 -I ^ -P $parallelization bash -c "hdfs dfs -cat ^ | grep -q '$string' && echo ^"
